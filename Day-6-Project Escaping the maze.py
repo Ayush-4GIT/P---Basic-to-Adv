ae0d2ad03eb274d9e6aa95486bdccb0560291044
Ayush-4GIT <ayush.mehta029@gmail.com>
2026-06-15 23:39:32 +0530

    feat: implement maze navigation logic for Day 6 project

---
+++ Day-6-Project Escaping the maze.py
@@ -0,0 +1,13 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        turn_right()
