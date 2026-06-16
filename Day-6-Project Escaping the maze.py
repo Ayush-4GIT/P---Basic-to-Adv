f7953919ce8a2d13c8d0fa957e120596a1c98262
Ayush-4GIT <ayush.mehta029@gmail.com>
2026-06-16 22:15:56 +0530

    feat: add commented exercise solutions for Reeborg's World maze levels

--- Day-6-Project Escaping the maze.py
+++ Day-6-Project Escaping the maze.py
@@ -1,13 +1,131 @@
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# # # Reeborg's World 
 
-while front_is_clear():
-    move()
-turn_left()
-while not at_goal():
-    if front_is_clear():
-        move()
-    else:
-        turn_right()
+# # # 1-Make a squar
+
+# # # def turn_around():
+# # #     turn_left()
+# # #     turn_left()
+# # #     turn_left()
+    
+# # # turn_left()
+# # # move()
+# # # turn_around()
+# # # move()
+# # # turn_around()
+# # # move()
+# # # turn_around()
+# # # move()
+# # # turn_around()
+
+
+# # # Hurddle level-1
+
+# # # def turn_around():
+# # #     turn_left()
+# # #     turn_left()
+# # #     turn_left()
+# # # def turn_front():
+# # #     move()   
+# # #     turn_left()
+# # #     move()
+# # #     turn_around()
+# # #     move()
+# # #     turn_around()
+# # #     move()
+# # #     turn_left()
+# # # for n in range (0, 6):
+# # #     turn_front()
+
+# # #Hurddle level - 2
+
+# # # def turn_around():
+# # #     turn_left()
+# # #     turn_left()
+# # #     turn_left()
+# # # def turn_front():
+# # #     move()   
+# # #     turn_left()
+# # #     move()
+# # #     turn_around()
+# # #     move()
+# # #     turn_around()
+# # #     move()
+# # #     turn_left()
+    
+# # # while not at_goal():
+# # #     turn_front()
+
+
+# # #Hurddle level - 3
+
+# # # def around_all():
+# # #     turn_left()
+# # #     turn_left()
+# # #     turn_left()
+
+# # # def follow_path():
+# # #     turn_left()
+# # #     move()
+# # #     around_all()
+# # #     move()
+# # #     around_all()
+# # #     move()
+# # #     turn_left()
+    
+
+# # # while not at_goal():
+# # #     if wall_in_front():
+# # #         follow_path()
+# # #     else:
+# # #         move()
+
+# # Huddle level - 4
+
+# # def around_all():
+# #     turn_left()
+# #     turn_left()
+# #     turn_left()
+
+# # def follow_path():
+# #     turn_left()
+# #     while wall_on_right():
+# #         move()
+# #     around_all()
+# #     move()
+# #     around_all()    
+# #     while front_is_clear():
+# #         move()
+# #     turn_left()
+        
+# # while not at_goal():
+# #     if wall_in_front():
+# #         follow_path()
+# #     else:
+# #         move()
+
+# Final - The MAZE
+
+# def around_all():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# while front_is_clear():
+#     move()
+# turn_left()
+        
+# while not at_goal():
+#     if right_is_clear():
+#         around_all()
+#         move()
+#     elif wall_on_right():
+#         turn_left()
+#     else:
+#         move()
+    
+        
+  
+
+
+    
