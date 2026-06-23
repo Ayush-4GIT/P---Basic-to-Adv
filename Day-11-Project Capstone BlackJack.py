049c70806458a24444aba49b05d79a64072173b2
Ayush-4GIT <ayush.mehta029@gmail.com>
2026-06-23 20:08:52 +0530

    Final: Blackjack game project

--- Day-11-Project Capstone BlackJack.py
+++ Day-11-Project Capstone BlackJack.py
@@ -6,20 +6,88 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-    new_card = deal_card()
-    user_cards.append(new_card)
-    computer_cards.append(new_card)
 
 def calculate_score(cards):
-
     if sum(cards) == 21 and len(cards) == 2:
         return 0
-    elif 11 in cards and sum(cards) > 21:
+    if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
-        
-    return sum[cards]
+
+    return sum(cards)
+
+def compare(u_score, c_score):
+    if u_score == c_score:
+        return "Draw"
+    elif c_score == 0:
+        return "Lose, Opponent has a  BlackJack"
+    elif u_score == 0:
+        return "Win with a BlackJack"
+    elif u_score > 21:
+        return "You went over, You Lose"
+    elif c_score > 21:
+        return "Opponent went over, You won"
+    elif u_score > c_score:
+        return "You Win"
+    else:
+        return "You Lose, Opponent win"
+
+
+def play_again():
+    print(r"""
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+""")
+    user_cards = []
+    computer_cards = []
+    user_score = -1
+    computer_score = -1
+    is_game_over = False
+
+
+
+    for _ in range(2):
+        new_card = deal_card()
+        user_cards.append(new_card)
+        computer_cards.append(new_card)
+
+    while not is_game_over:
+
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"User's cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Do you want to draw again 'y' or end 'n'?: ").lower()
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+        print(f"Computer's cards {computer_cards}, current score: {computer_score} and "
+              f"User's cards {user_cards}, current score: {user_score}")
+
+    print(compare(u_score = user_score, c_score = computer_score))
+
+while input("Do you want to play a game of BlackJack?(y/n): ") == "y".lower():
+    print("\n" * 20)
+    play_again()
+
+
+
+
+
+
+
