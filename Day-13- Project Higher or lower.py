014bcf4ab0cae7b0d3eea29e42ee5335d85960d9
Ayush-4GIT <ayush.mehta029@gmail.com>
2026-06-24 23:11:48 +0530

    feat: implement random selection and display logic for Higher or Lower game choices

--- Day-13- Project Higher or lower.py
+++ Day-13- Project Higher or lower.py
@@ -1,1 +1,11 @@
 from Datasets.Day_13 import data
+import random 
+
+choice_A = random.choice(data)
+print(f"Compare A: {choice_A['name']}, a {choice_A['description']}, from {choice_A['country']}")
+
+choice_B = random.choice(data)
+print(f"Compare B: {choice_B['name']}, a {choice_B['description']}, from {choice_B['country']}")
+
+user_input = input("Who has more followers? Type 'A' or 'B': ").upper()
+
